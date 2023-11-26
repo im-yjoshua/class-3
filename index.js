314580b60905066dc928ba56249a984852822904
im-yjoshua <145139175+im-yjoshua@users.noreply.github.com>
2023-11-26 16:26:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+// var _name:string = "Joshua";
+// var num1: number = 10;
+// var num2: number = 10;
+// // num2 += num1;
+// console.log("The number is: " , num2);
+// // Assignment Operator
+// console.log(`This is num1: ${num1}`)
+// console.log(`This is num2: ${num2}`)
+// var num3: boolean = num1 == num2;
+// console.log(num3);
+// var age = 20;
+// var test1 = "Pakistani";
+// var test2 = age >= 18 
+// var value = test1 && test2;
+// console.log("Test: " , value);
+// var lisence :string = 'learner';
+// var test: boolean = lisence === 'learner' || lisence === 'driving';
+// console.log("Test:", test)
+// String Operator:
+var _name = "Y";
+var _name2 = "J";
+var _sentence = "My name is ";
+console.log(_sentence, _name + _name2);
+console.log(`My name is ${_name + _name2}`);
---
+++ index.ts
@@ -0,0 +1,38 @@
+// var _name:string = "Joshua";
+// var num1: number = 10;
+// var num2: number = 10;
+
+// // num2 += num1;
+
+// console.log("The number is: " , num2);
+
+// // Assignment Operator
+
+// console.log(`This is num1: ${num1}`)
+// console.log(`This is num2: ${num2}`)
+
+
+// var num3: boolean = num1 == num2;
+// console.log(num3);
+
+// var age = 20;
+// var test1 = "Pakistani";
+// var test2 = age >= 18 
+// var value = test1 && test2;
+
+// console.log("Test: " , value);
+
+// var lisence :string = 'learner';
+// var test: boolean = lisence === 'learner' || lisence === 'driving';
+
+// console.log("Test:", test)
+
+// String Operator:
+
+var _name :string = "Y";
+var _name2: string = "J";
+var _sentence: string = "My name is ";
+
+console.log(_sentence, _name + _name2)
+
+console.log(`My name is ${_name +_name2}`)
